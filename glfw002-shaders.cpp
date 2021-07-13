ee51382fd26159e10183760dfad5bb40b741e2cb
SalHe <SalHe@qq.com>
2021-07-13 11:49:11 +0800

    more vertex attribute

---
+++ glfw002-shaders.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char const *argv[])
+{
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Glfw002Shaders", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to load GL" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow *win, int width, int height)
+                                   { glViewport(0, 0, width, height); });
+
+    // int nrAttributes;
+    // glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    // std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
+    // Prepare the shader program
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    {
+        const char *source = "#version 330 core\n"
+                             "layout (location = 0) in vec3 aPos;\n"
+                             "layout (location = 1) in vec3 aColor\n;"
+                             "\n"
+                             "out vec3 ourColor;\n"
+                             "\n"
+                             "void main()\n"
+                             "{\n"
+                             "   gl_Position = vec4(aPos, 1.0);\n"
+                             "   ourColor = aColor;\n"
+                             "}\n";
+        glShaderSource(vertexShader, 1, &source, nullptr);
+        glCompileShader(vertexShader);
+        GLint success;
+        char log[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, nullptr, log);
+            std::cout << "Error when compile vertex shader: " << log << std::endl;
+        }
+    }
+    {
+        const char *source = "#version 330 core\n"
+                             "out vec4 FragColor;\n"
+                             "in vec3 ourColor;\n"
+                             "\n"
+                             "void main()\n"
+                             "{\n"
+                             "   FragColor = vec4(ourColor, 1.0);\n"
+                             "}\n";
+        glShaderSource(fragmentShader, 1, &source, nullptr);
+        glCompileShader(fragmentShader);
+        GLint success;
+        char log[512];
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragmentShader, 512, nullptr, log);
+            std::cout << "Error when compile fragment shader: " << log << std::endl;
+        }
+    }
+
+    GLuint shaderProgram = glCreateProgram();
+    glUseProgram(shaderProgram);
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    {
+        GLint success;
+        char log[512];
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shaderProgram, 512, nullptr, log);
+            std::cout << "Error when link shader program: " << log << std::endl;
+        }
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Prepare the vertex
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    {
+        float vertices[] = {
+            // 位置              // 颜色
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // 右下
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 左下
+            0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // 顶部
+        };
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            break;
+        }
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+    return 0;
+}
