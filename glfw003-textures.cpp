3bcc2b3138b42d209533be3a013772c4ea60d43b
SalHe <SalHe@qq.com>
2021-07-13 20:08:45 +0800

    awesomeface

--- glfw003-textures.cpp
+++ glfw003-textures.cpp
@@ -79,12 +79,13 @@ int main(int argc, char const *argv[])
                              "in vec3 ourColor;\n"
                              "in vec2 TexCoord;\n"
                              "\n"
-                             "uniform sampler2D ourTexture;\n"
+                             "uniform sampler2D texture1;\n"
+                             "uniform sampler2D texture2;\n"
                              "\n"
                              "void main()\n"
                              "{\n"
-                            //  "   FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);\n"
-                             "   FragColor = texture(ourTexture, TexCoord);\n"
+                             //  "   FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);\n"
+                             "   FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);\n"
                              "}\n";
         glShaderSource(fragmentShader, 1, &source, nullptr);
         glCompileShader(fragmentShader);
@@ -127,15 +128,15 @@ int main(int argc, char const *argv[])
     {
         float vertices[] = {
             //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-             0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-             0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // 右上
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 右下
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // 左下
+            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // 左上
         };
 
         int indices[] = {
-            0, 1, 3,  // first Triangle
-            1, 2, 3   // second Triangle
+            0, 1, 3, // first Triangle
+            1, 2, 3  // second Triangle
         };
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -148,22 +149,40 @@ int main(int argc, char const *argv[])
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
         glEnableVertexAttribArray(2);
-        
     }
 
     // Prepare the texture
-    GLuint texture;
-    glGenTextures(1, &texture);
+    GLuint texture1, texture2;
+    glGenTextures(1, &texture1);
+    glGenTextures(1, &texture2);
     {
-        glBindTexture(GL_TEXTURE_2D, texture);
+        int width, height, channels;
+        unsigned char * data;
+
+        // texture 1
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        data = stbi_load("container.jpg", &width, &height, &channels, 0);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+
+        // texture 2
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        int width, height, channels;
-        unsigned char *data = stbi_load("container.jpg", &width, &height, &channels, 0);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        stbi_set_flip_vertically_on_load(true);
+        data = stbi_load("awesomeface.png", &width, &height, &channels, 0);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
         stbi_image_free(data);
     }
@@ -179,7 +198,12 @@ int main(int argc, char const *argv[])
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
+        glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
